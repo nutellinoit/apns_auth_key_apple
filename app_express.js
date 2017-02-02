f711ebe8394969ea8d2646a0b7ef55bfa323271e
Samuele Chiocca <samuelechiocca@MacBookiSamuele.localdomain>
2017-02-02 08:41:29 +0100

    second commit

--- app_express.js
+++ app_express.js
@@ -24,9 +24,9 @@ app.post('/sendpush', function(request, response) {
 			token: {
 				key: 'apns.p8',
 				// Path to the key p8 file
-				keyId: '83RESLEU3V',
+				keyId: 'XXXXXXX',
 				// The Key ID of the p8 file (available at https://developer.apple.com/account/ios/certificate/key)
-				teamId: 'CHAWTB35L9',
+				teamId: 'XXXXXXX',
 				// The Team ID of your Apple Developer Account (available at https://developer.apple.com/account/#/membership/)
 			},
 			production: true // Set to true if sending a notification to a production iOS app
